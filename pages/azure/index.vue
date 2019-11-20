de3c420a61a8dcf9736aa56822f2b54957970fc9
AcE <goth14wx@gmail.com>
2019-11-20 16:13:18 -0600

    ultimo commit, fin del proyecto portafolio ciclo 3 para GSW 2019

--- pages/azure/index.vue
+++ pages/azure/index.vue
@@ -10,6 +10,214 @@
                 <cardParagraph :textCard="texto" :classAdd="'card06'"  :fancy="false"/>
             </v-col>
         </v-row>
+        <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/1.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/2.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/3.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/4.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/5.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/6.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/7.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/8.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/9.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/10.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/11.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/12.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/13.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/14.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/15.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/16.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/17.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/18.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/19.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/20.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/21.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/22.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/23.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/24.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/25.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
+              <v-row align="center" justify="center" data-aos="fade-down-right">
+            <v-col class="text-center">
+                <img
+      src="~~/assets/img/azure/26.png"
+      width="900px"
+    />
+            </v-col>
+             </v-row>
     </v-container>
 </template>
 
