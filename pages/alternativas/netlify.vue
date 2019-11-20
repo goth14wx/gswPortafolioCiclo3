72aadfdacdd637173498a9f31529467d1d0b4ba2
AcE <goth14wx@gmail.com>
2019-11-20 02:06:47 -0600

    cambios penultimos

--- pages/alternativas/netlify.vue
+++ pages/alternativas/netlify.vue
@@ -11,7 +11,7 @@
  </v-row>
  <v-row>
        <v-col>
-        <iframe width="100%" height="400px" src="https://www.youtube.com/embed/aR3wjq2QDG0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="100%" height="400px"  src="https://www.youtube.com/embed/152-yYIMkho" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </v-col>
  </v-row>
  <v-row>
